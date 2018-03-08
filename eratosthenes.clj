9b8b6253fa0a7980c2ca3b41144f4bdb15939165
Clemens Novak <clemens@familie-novak.net>
2018-03-08 16:46:36 +0100

    and more fun...

--- eratosthenes.clj
+++ eratosthenes.clj
@@ -40,3 +40,39 @@
 (aset *ba* 3 true)
 (aget *ba* 3)
 (filter #(not (aget *ba* %)) (range 1 10))
+
+
+
+; new trial with a bit array (more or less a direct translation from julia)
+; does not work
+(defn primes-v2 [N]
+  (let [ba (boolean-array (+ N 1))]
+    (aset ba 1 true)
+    (loop [i 1]
+      (if (aget ba i)
+        (let [j (* 2 i)]
+          (if (> j N)
+          ba
+          (loop [j (* 2 i)]
+            (aset ba j true)
+            (recur (+ j i)))))))))
+
+
+; this works :-)
+(defn p1 [i primes N] ; this function si kick-started by primes-v4 below
+  (if (some #(= i %) primes) ; we want to check whether i is in primes; contains? does not work (see clojure help)
+      (p1 (inc i) primes N)  ; increment i and start again
+      (if (> i N) ; are we finished?
+        primes    ; yes -> return primes; actually it's composite numbers
+        (p1 (inc i) (concat primes (range (* 2 i) N i)) N)))) ; not finished: start again, increment i and add to primes list
+
+; kickstart for function above
+(defn primes-v4 [N]
+  (let [comp (into #{} (p1 2 (range 2 N 2) N))]  ; 2 is a prime and primes = 2,4,6,8... N
+    (sort (clojure.set/difference (set (range 1 N)) comp))))
+
+(primes-v4 20)
+
+
+
+
