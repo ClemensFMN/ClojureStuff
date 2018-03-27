4197ce04dc7c326256026e702f07ae7ddd944ab5
Clemens Novak <clemens@familie-novak.net>
2018-03-27 16:27:37 +0200

    http + json cont'd

--- http-client/src/http_client/core.clj
+++ http-client/src/http_client/core.clj
@@ -3,14 +3,24 @@
   	        [cheshire.core :refer :all])
   (:gen-class))
 
-; on the repl do
-; (require '[clj-http.client :as client])
-; (client/get "http://www.httpbin.org/ip")
+
+; if a lein repl is executed, we seem to be in the http-client.core namespace
+; and can execute commands (including the ones from the imported libraries)
+; changing via namespaces works via (ns namespace):
+; (ns user) -> here we cannot call (client/get "http://httpbin.org/ip") because the lib is not importe din this namespace
+; (ns http-client.core) -> here we can call (client/get ...) because we import the lib into this namespace
+; when we edit the file, we can reload it into the REPL via
+; (use 'http-client.core :reload) or (use 'http-client.core :reload-all)
+
+(defn f1 [x] (+ x 2))
 
 
 (defn -main []
-  (let [dta (:body (client/get "http://httpbin.org/ip"))]
-  	(println (parse-string dta)))); "{:request-time 567}")))
-  		;(println dta)));(parse-string dta))))
+  (let [dta (:body (client/get "http://httpbin.org/ip"))
+        prsd (parse-string dta true)]
+  	(do
+      (println prsd)
+      (get prsd :origin))))
 
-; (parse-string "{\"foo\":\"bar\"}")
+; (println (parse-string "{\"age\":45}" true))
+; (get (parse-string "{\"age\":45, \"name\":\"dkdjf\", \"lst\":[1,3,4]}" true) :lst)
