c72347fa38671872c05b8826becf18c87bf843c8
Clemens Novak <clemens@familie-novak.net>
2018-04-19 21:18:43 +0200

    some more transient stuff

--- transient-stuff.clj
+++ transient-stuff.clj
@@ -5,3 +5,12 @@
       (persistent! v))))
 
 (vrange 5)
+
+
+(defn my-fibb [N]
+  (loop [i 0 fib (transient [1 1])] ; we create a counter i and a transient vector initialized with the first two fib numbers
+  	(if (< (+ i 2) N) ; correct the counter by 2
+  	  (recur (inc i) (conj! fib (+ (get fib i) (get fib (inc i))))) ; inc counter + add next fibonacci number to transient
+  	  (persistent! fib)))) ; done? -> make transient persistent & return it
+
+(my-fibb 5)
