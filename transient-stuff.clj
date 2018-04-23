51b13267266b7d81c6f23bf867898b84ab80fffc
Clemens Novak <clemens@familie-novak.net>
2018-04-23 21:44:24 +0200

    3n+1 transient stuff

--- transient-stuff.clj
+++ transient-stuff.clj
@@ -14,3 +14,22 @@
   	  (persistent! fib)))) ; done? -> make transient persistent & return it
 
 (my-fibb 5)
+
+; the 3n+1 sequence
+; create one step of the 3n+1 sequence
+(defn one-step [n]
+  (cond
+    (even? n) (/ n 2)        ; n even -> n/2
+    (odd? n) (+ (* 3 n) 1))) ; n odd -> 3n+1
+
+; create the sequence with start value x0
+(defn three-n-plus-one [x0]
+   (loop [i 0 sq (transient []) xn x0] ; setup a loop with zero length, (transient) empty seq  and val = x0
+      (if (= xn 1) ; one?
+         {:runs (inc i) :sequence (persistent! (conj! sq 1))} ; -> yes, return map with sequence (persistent!) and length
+         (recur (inc i) (conj! sq xn) (one-step xn))))) ; otherwise, loop with increased length and next val conj'd to sq
+
+(three-n-plus-one 15)
+
+
+
