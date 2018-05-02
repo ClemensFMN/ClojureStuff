c9c5b246579d4769ba955641a441d940f6a3f5e2
Clemens Novak <clemens@familie-novak.net>
2018-05-02 06:45:33 +0200

    own sudok, cont'd

--- sudoku_2.clj
+++ sudoku_2.clj
@@ -2,10 +2,27 @@
 ; like in sudoku_1, we model the board as vector and not as map
 ; this allows simpler debugging as a map is not sorted
 
+
+(def test-puzzle
+  [0 0 3 0 2 0 6 0 0
+   9 0 0 3 0 5 0 0 1
+   0 0 1 8 0 6 4 0 0
+   0 0 8 1 0 2 9 0 0
+   7 0 0 0 0 0 0 0 8
+   0 0 6 7 0 8 2 0 0
+   0 0 2 6 0 9 5 0 0
+   8 0 0 2 0 3 0 0 9
+   0 0 5 0 1 0 3 0 0])
+
+
 (def all-puzzle-coords
   (for [row (range 0 9)
         col (range 0 9)]
     [col row]))
+
+(defn puzzle-index [col row]
+  (+ col
+     (* row 9)))
 
 (defn convert-puzzle-to-map
   "convert a puzzle in string presentation to a map col/val -> value(s)"
@@ -19,8 +36,8 @@
                   [[col row] [value]])))))
   
 (def test-puzzle-map (convert-puzzle-to-map test-puzzle))
-(test-puzzle-map [0 0])
-(test-puzzle-map [2 0])
+(test-puzzle-map [0 0]) ; -> [1 2 3 4 5 6 7 8 9]
+(test-puzzle-map [2 0]) ; -> [3]
 
 
 (defn row-coords
@@ -73,11 +90,11 @@
   (distinct
     (flatten
       (concat
-        (filter #(= 1 (count %)) (map #(test-puzzle-map %) (row-coords col row)))
-        (filter #(= 1 (count %)) (map #(test-puzzle-map %) (col-coords col row)))
-        (filter #(= 1 (count %)) (map #(test-puzzle-map %) (grid-coords col row)))))))
+        (filter #(= 1 (count %)) (map #(puzzle %) (row-coords col row)))
+        (filter #(= 1 (count %)) (map #(puzzle %) (col-coords col row)))
+        (filter #(= 1 (count %)) (map #(puzzle %) (grid-coords col row)))))))
 
-(get-relevant-neighbours test-puzzle-map 0 0)
+(get-relevant-neighbours test-puzzle-map 0 0) ; -> (3 2 6 9 7 8 1)
 
 (defn constraint-prop-position
   "constraint propagation of puzzle for a specific col/row position - returns an updated puzzle!"
@@ -96,110 +113,44 @@
 (defn constraint-prop-round
   "one round of constraint propagation; i.e. going over all positions once"
   [puzzle]
-  (loop [pos all-puzzle-coords p puzzle]
+  (reduce (fn [board [col row]]
+    ;(println col row board)
+    (constraint-prop-position board col row))
+    puzzle
+    all-puzzle-coords))
 
+(def one-round (constraint-prop-round test-puzzle-map))
+(filter #(= 1 (count (second %))) one-round)
 
-    )
+; it is weird, that (one-round [6 7] is still (1 7) as (one-round [6 1]) = (7 8) & (one-round [6 4]) became 1 in the last round of constraint prop
+; all other vales in this column are fixed - why isn't 6 7 updated??
 
-  )
+(constraint-prop-position one-round 6 7)
+(get-relevant-neighbours one-round 6 7)
 
+; FOUND - there was an ugly bug in get-relevant-neighbours & constraint-prop-round...
 
+((constraint-prop-round test-puzzle-map) [0 0])
 
+(= test-puzzle-map (constraint-prop-round test-puzzle-map))
+(= (constraint-prop-round test-puzzle-map) (constraint-prop-round (constraint-prop-round test-puzzle-map)))
 
-
-
-
-(defn puzzle-index
-  "convert col/row value to vector index"
-  [col row]
-  (+ col
-     (* row 9)))
-
-(defn puzzle-row
-  "get values from puzzle in row"
-  [puzzle row]
-  (for [col (range 0 9)] ; run over cols
-    (puzzle (puzzle-index col row)))) ; and collect the puzzle values
-
-(defn puzzle-column
-  "get values from puzzles in column"
-  [puzzle col]
-  (for [row (range 0 9)] ; run over rows
-    (puzzle (puzzle-index col row)))) ; and collect the puzzle values
-
-(defn subgrid-coords
-  "convert col/row to subgrid number"
-  [col row]
-  (map (fn [n] (int (/ n 3)))
-       [col row]))
-
-(defn puzzle-subgrid
-  "get values from puzzle's 3x3 subgrid belonging to col/row"
-  [puzzle col row]
-  (let [[c r] (subgrid-coords col row)] ; find subgrid number
-    (let [start-col (* 3 c)
-           start-row (* 3 r)]
-       (for [row (range start-row (+ 3 start-row)) ; iterate over corresponding row
-             col (range start-col (+ 3 start-col))] ; and cols
-         (puzzle (puzzle-index col row)))))) ; and collect puzzle values
-
-(puzzle-subgrid almost-complete-puzzle 0 0) ; -> (0 0 3 9 6 7 2 5 1)
-(puzzle-subgrid almost-complete-puzzle 0 1) ; -> (0 0 3 9 6 7 2 5 1)
-(puzzle-subgrid almost-complete-puzzle 0 3) ; -> (5 4 8 7 2 9 1 3 6)
-(puzzle-subgrid almost-complete-puzzle 1 0) ; -> (0 0 3 9 6 7 2 5 1)
-(puzzle-subgrid almost-complete-puzzle 3 0) ; -> (9 2 1 3 4 5 8 7 6)
-
-(defn constraint-prop-position
-  "Get all allowed values for a col and row index on a puzzle."
-  [puzzle col row]
-  (apply disj
-         (set (range 1 10))
-         (concat (puzzle-column puzzle col)
-                 (puzzle-row puzzle row)
-                 (puzzle-subgrid puzzle col row))))
-
-(allowed-values test-puzzle 0 0) ; -> #{4 5}
-(allowed-values test-puzzle 1 1) ; -> #{7 4 6 2 8}
-
-
-(defn allowed-values-map
-  "takes a puzzle and returns a map (col/row) -> allowed values"
+(defn constraint-prop
+  "constraint propagation until nothing changes anymore"
   [puzzle]
-  (into {} 
-    (for [row (range 0 9) ; run over all rows
-                 col (range 0 9)] ; and cols
-             (let [values (allowed-values puzzle col row)] ; get the allowed values
-                 [[col row] values])))) ; and stuff them into a map
+  (loop [i 0 pzl puzzle]
+    (let [new-pzl (constraint-prop-round pzl)]
+      (if (= pzl new-pzl)
+        {:sol new-pzl :iters i}
+      (recur (inc i) new-pzl)))))
 
-(defn const-prop-pos
-  "perform constraint propagation for a specific position"
-  )
+(def cp-complete (constraint-prop test-puzzle-map))
 
 
 
-(defn print-puzzle
-  "print puzzle to screen"
-  [puzzle]
-  (doseq [[col row] all-puzzle-coords]
-    (cond  (= 0 col row) (print "[")
-           (= 0 col) (print " "))
-    (print (puzzle (puzzle-index col row)))
-    (if-not (= 8 col) (print " "))
-    (if  (= 8 col row) (print "]"))
-    (if (= 8 col) (print "\n"))))
 
 
 
-(def test-puzzle
-  [0 0 3 0 2 0 6 0 0
-   9 0 0 3 0 5 0 0 1
-   0 0 1 8 0 6 4 0 0
-   0 0 8 1 0 2 9 0 0
-   7 0 0 0 0 0 0 0 8
-   0 0 6 7 0 8 2 0 0
-   0 0 2 6 0 9 5 0 0
-   8 0 0 2 0 3 0 0 9
-   0 0 5 0 1 0 3 0 0])
 
 (def almost-complete-puzzle
   [0 0 3 9 2 1 6 5 7
