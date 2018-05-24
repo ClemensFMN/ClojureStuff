1a25ac38d26f3fe2b07c1394876a2c1bcb9b20ef
Clemens Novak <clemens@familie-novak.net>
2018-05-24 16:52:40 +0200

    proj euler #59

--- project_euler_p59.clj
+++ project_euler_p59.clj
@@ -1,15 +1,26 @@
+; Project Euler Problem #59 - XOR-"Encryption" with a key-phrase 
+
+
 (def mystring "Hallo Welt, wie geht es Dir?")
 
 (def key-phrase "asdf")
 
+; repeat the key-phrase indefintely
+(defn key-phrase-rep [key-phrase]
+  (flatten (repeat (seq (char-array key-phrase)))))
+
+; zip the repeated key-phrase with the string to encrypt -> the repeated key-phrase is trimmed to the length of the string
+(defn mystring-key-phrase [s kp]
+  (map vector s (key-phrase-rep kp)))
+
+; encryption using xor
+(defn encrypt [s kp]
+ (map #(bit-xor (int (first %)) (int (second %))) (mystring-key-phrase s kp)))
 
 
-(def key-phrase-rep (flatten (repeat (seq (char-array key-phrase)))))
+(encrypt mystring key-phrase)
 
-(def mystring-key-phrase (map vector mystring key-phrase-rep))
+(map char (encrypt mystring key-phrase))
 
-
-(def encrypted-string (map #(bit-xor (int (first %)) (int (second %))) mystring-key-phrase))
-
-(map char encrypted-string)
+(map char (encrypt (encrypt mystring key-phrase) key-phrase))
 
