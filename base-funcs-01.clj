78e2e950e74400d553b68f13a7580701582d34e6
Clemens Novak <clemens@familie-novak.net>
2020-05-11 11:58:49 +0200

    modified RLE example

--- base-funcs-01.clj
+++ base-funcs-01.clj
@@ -9,17 +9,45 @@
 ; returns a new value
 (partition-by #(mod % 2) my-lst)
 
+
+; RLE
+; ============
+
 ; we can use partition to perform run length encoding (RLE)
 ; lets partition using the identity function -> every "new" value in the list
 ; splits the list
-(def p (partition-by identity my-lst))
+(defn pid [lst]
+  (partition-by identity my-lst))
 
-; build a list with the #elements and the first element
-(defn rle [x]
+(pid my-lst)
+
+; return a list with the #elements and the first element
+(defn rle-cnt [x]
   (list (count x) (first x)))
 
-; and map the rle function over the partitioned list
-(def rle-my-lst (map rle p))
+(rle-cnt '(1 1 1 1)) ; -> (4,1)
+
+; and map the rle function over the partitioned list => this yields the run-length encoded list
+(defn my-rle [lst]
+  (map rle-cnt (pid lst)))
+
+(def rle-res (my-rle my-lst))
+
+; Now go backwards
+
+; repeat the second element of x the first element of x times  
+(defn rep [x]
+  (repeat (first x) (second x)))
+
+(rep '(2 4)) ; -> (4,4)
+
+(defn dec-rle [r]
+  (flatten ; and flatten the thing afterwards
+   (map rep r))) ; repeat each element of the RLE lst
+
+(dec-rle rle-res)
+
+; ========================
 
 ; "duplicate" items
 (defn dbl [x] (list x x))
